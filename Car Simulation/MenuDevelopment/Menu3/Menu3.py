523950af477bf017626e99d9477bf632b3a9e8d4
chilliconkearney <59411530+chilliconkearney@users.noreply.github.com>
2022-12-12 21:51:19 +0000

    Update Menu3.py

--- Car Simulation/MenuDevelopment/Menu3/Menu3.py
+++ Car Simulation/MenuDevelopment/Menu3/Menu3.py
@@ -17,7 +17,7 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 class Menu: # used for the different submenus in the UI
-    def __init__(self, name, titlesList:list, subroutines:list) -> None:
+    def __init__(self, name, titlesList:list, subroutines=[]) -> None:
         self.name = name
         self.titles = titlesList 
         self.subroutines = subroutines
@@ -42,7 +42,7 @@ class car:
 
         self.driver = DriveBase(self.motorD,self.motorA,31,190) # wheels have diameter of 31mm and a drivebase width of 190mm    
 
-        self.colsense = ColorSensor(Port.S4)
+        #self.colsense = ColorSensor(Port.S4) # Left out as wont work right now, FIX THIS!!!!!
         self.driver.settings(straight_speed=200) # sets the cars speed to 200mm /s
     
     def CalTurning(self):
@@ -57,25 +57,25 @@ class car:
         time.sleep(1)
 
     def CalTrackLine(self):
-        pass
+        print("Tracking Line")
 
     def SetMotorSpeed(self):
-        pass
+        print("settings motor speed")
 
     def SetLogType(self):
-        pass
+        print("setting log type")
 
     def SetLogInterval(self):
-        pass
+        print("setting log interval")
 
     def RunAll(self):
-        pass
+        print("running all")
 
     def RunSearch(self):
-        pass
+        print("running search")
 
     def RunSolved(self):
-        pass
+        print("running solved")
 
 
 class Main: # will hold the main section of the program. useful for dropping in different main loops
@@ -84,10 +84,16 @@ class Main: # will hold the main section of the program. useful for dropping in 
 
     def runMain(self): # the main section of the code is here vvv
         Car = car()
-        MainMenu = Menu("Main", ["Run Menu", "Calibration Menu", "Settings Menu", "Quit"], [RunMenu, CalMenu, SetMenu, self.Exit])
-        RunMenu = Menu("Run", ["Run All", "Run Search", "Run Solve", "Main Menu"], [Car.RunAll, Car.RunSearch, Car.RunSolved, MainMenu])
-        CalMenu = Menu("Calibration", ["Track Line", "Turning", "Main Menu"], [Car.CalTrackLine, Car.CalTurning, MainMenu])
-        SetMenu = Menu("Settings", ["Motor Speed", "Logging Type", "Logging Intervals", "Main Menu"], [Car.SetMotorSpeed, Car.SetLogType, Car.SetLogInterval, MainMenu])
+        MainMenu = Menu("Main", ["Run Menu", "Calibration Menu", "Settings Menu", "Quit"])
+        RunMenu = Menu("Run", ["Run All", "Run Search", "Run Solve", "Main Menu"])
+        CalMenu = Menu("Calibration", ["Track Line", "Turning", "Main Menu"])
+        SetMenu = Menu("Settings", ["Motor Speed", "Logging Type", "Logging Intervals", "Main Menu"])
+        
+        MainMenu.subroutines = [RunMenu, CalMenu, SetMenu, self.Exit]
+        RunMenu.subroutines = [Car.RunAll, Car.RunSearch, Car.RunSolved, MainMenu]
+        CalMenu.subroutines = [Car.CalTrackLine, Car.CalTurning, MainMenu]
+        SetMenu.subroutines = [Car.SetMotorSpeed, Car.SetLogType, Car.SetLogInterval, MainMenu]
+        
         currentMenu = MainMenu
         pointer = 0
 
@@ -103,14 +109,17 @@ class Main: # will hold the main section of the program. useful for dropping in 
                 time.sleep(0.3)
             if Button.CENTER in Car.buttons.pressed():
                 try: # will try to run the subroutine in the current menu
-                    if isinstance(currentMenu[pointer], Menu):
-                        Car.screen.clear()
-                        currentMenu.draw(Car.screen, pointer)
-                        time.sleep(1)
+                    if isinstance( currentMenu.subroutines[pointer],Menu):
+                        currentMenu = currentMenu.subroutines[pointer]
+                        pointer = 0
+
+                    else:
+                        currentMenu.subroutines[pointer]()
                     
                     
-                except: 
-                    pass
+                except Exception as e: 
+                    print("hmmm, this didnt work")
+                    print("here is why:", e)
 
             time.sleep(0.1)
 
