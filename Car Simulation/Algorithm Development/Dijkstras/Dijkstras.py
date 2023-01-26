23ec457ae584dc37ee6f9f5c872f5567566bb9c8
chilliconkearney <59411530+chilliconkearney@users.noreply.github.com>
2023-01-26 20:36:59 +0000

    PAIN
    PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN PAIN

--- Car Simulation/Algorithm Development/Dijkstras/Dijkstras.py
+++ Car Simulation/Algorithm Development/Dijkstras/Dijkstras.py
@@ -11,38 +11,72 @@ class Vertex():
 class Graph():
     def __init__(self, end_label):
         self.network = []
-        self.label_index = 0
+        self.label_index = 65 # 65 in unicode is A
         self.end = end_label
 
-    def add_vertex(self, left, right, rear, front):
+    def add_vertex(self, left = [None,None], right = [None,None], rear = [None, None], front = [None, None]):
         Vert = Vertex(chr(self.label_index), left, right, rear, front)
         self.network.append(Vert)
+        self.label_index += 1
 
 
     def Dijkstras(self):
         
         Queue = self.network
         Permanent = []
+        dirList = ["Left", "Right", "Rear", "Front"]
         
         for i in range(1, len(self.network)): # setting up the initial values of the distance from the start node. 
             Queue[i].totalweight = math.inf   
         
-        while self.find_vertex(Queue, self.end).permanent != True: # the main loop which checks  all the vertices
+        while len(Queue) > 0: # the main loop which checks  all the vertices
             
-            current_vertex = Queue.pop[0] # similar to the dequeue in static languages
+            self.bubble(Queue)
+            
+            current_vertex = Queue.pop(0) # similar to the dequeue in static languages
+            current_vertex.permanent = True
+
+            print("\n\n {}".format(current_vertex.label))
+            for direction in dirList:
+                print("{}: {}".format(direction, current_vertex.adjacencyDict[direction]))
+            print(current_vertex.totalweight) 
+
             Permanent.append(current_vertex)
 
-            dirList = ["Left", "Right", "Rear", "Front"]
-
             for i in range(0,4): # iterates from 0 to 3, stops when at 4
-                dirVertex = self.find_vertex()
+                dirVertexLabel = current_vertex.adjacencyDict[dirList[i]][0] # finds the vertex in the direction
+
+                try:
+                    if self.find_vertex(Queue, dirVertexLabel).totalweight > current_vertex.totalweight + current_vertex.adjacencyDict[dirList[i]][1]:
+                        self.find_vertex(Queue, dirVertexLabel).totalweight = current_vertex.totalweight + current_vertex.adjacencyDict[dirList[i]][1]
+                except Exception as e:
+                    print(e)
+   
+        
+        # Output the final path to follow
+        # For now, this will just output all the vertices with the links
+
+        print("WOOOOOO, YOU GOT THIS FAR!!!!")
+
+        for item in Permanent:
+            print("\n\n {}".format(item.label))
+            for direction in dirList:
+                print("{}: {}".format(direction, item.adjacencyDict[direction]))
+            print(item.totalweight)             
         
     def find_vertex(self, Queue, label):
         for item in Queue:
             if item.label == label:
                 return item # returns the vertex
 
-        return Queue[len(Queue)-2]  #  returns the final value of the queue   
+        try:
+            return Queue[len(Queue)-2]  #  returns the final value of the queue   
+        except:
+            print(len(Queue))
+            print(label)
+            for thing in Queue:
+                print(thing.label)
+            input()
 
     def bubble(self, queue):
             swaps = 1
@@ -55,3 +89,16 @@ class Graph():
                         swaps += 1
 
             return queue # returns the sorted version of queue
+
+
+myNetwork = Graph("D")
+
+myNetwork.add_vertex(right=["B", 4]) # A
+myNetwork.add_vertex(left=["A", 4], rear=["C", 4]) # B
+myNetwork.add_vertex(front=["B", 4], right=["E", 4], rear=["D", 4]) # C
+myNetwork.add_vertex(front=["C", 4]) # D
+myNetwork.add_vertex(left=["C", 4]) # E
+
+
+myNetwork.Dijkstras()
+input()
