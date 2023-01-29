96d3cbd5eb88a85a471b1bf4913fe95bc4c1ab2e
chilliconkearney <59411530+chilliconkearney@users.noreply.github.com>
2023-01-29 10:48:50 +0000

    2nd iteration Dijkstra's

--- Car Simulation/Algorithm Development/Dijkstras/Dijkstras.py
+++ Car Simulation/Algorithm Development/Dijkstras/Dijkstras.py
@@ -8,103 +8,78 @@ class Vertex():
         self.adjacencyWeights = [math.inf, math.inf, math.inf, math.inf] # has all the connected arc weights set to infinity
         self.totalweight = math.inf
         self.permanent = False
+        self.previousVertex = ""
+        self.previousDir = 0
 
     def update_adjacency(self, input = [[],[],[],[]]):
         for i in range(0,4):
             try:
-                if type(input[0][0]),type(input[0][1]) == str,float:
-                    pass
-        
-        
-        
-        
-        if type(left[0]) == str and type(left[1]) == float: # Left
-            self.adjacencyLabels[0] = left[0]
-            self.adjacencyWeights[0] = left[1]
-        
-        if type(left[0]) == str and type(left[1]) == float: # Right
-            self.adjacencyLabels[1] = right[0]
-            self.adjacencyWeights[1] = right[1]
-        
-        if type(left[0]) == str and type(left[1]) == float: # Rear
-            self.adjacencyLabels[1] = rear[0]
-            self.adjacencyWeights[1] = rear[1]
-        
-        if type(left[0]) == str and type(left[1]) == float: # Front
-            self.adjacencyLabels[1] = front[0]
-            self.adjacencyWeights[1] = front[1]
+                if len(input[i]) > 0:
+                    self.adjacencyLabels[i] = str(input[i][0])
+                    self.adjacencyWeights[i] = int(input[i][1])
+            except:
+                print("one of your values isn't the correct type")
             
-        
-
 
 class Graph():
     def __init__(self, end_label):
         self.network = []
-        self.label_index = 65 # 65 in unicode is A
+        self.label_index = 65 # 65 in unicode is A, works as an offset
         self.end = end_label
+        self.nullVertex = Vertex("*")
 
     def add_vertex(self, left = [], right = [], rear = [], front = []):
         Vert = Vertex(chr(self.label_index))
-        try:
-            self.network.append(Vert)
-        self.label_index += 1
+        Vert.update_adjacency([left,right,rear,front])
+        self.network.append(Vert)
+        self.label_index += 1 # increments the label by 1 e.g A -> B
 
 
     def Dijkstras(self):
-        
         Queue = self.network
         Permanent = []
-        dirList = ["Left", "Right", "Rear", "Front"]
         
         Queue[0].totalweight = 0 # sets the initial vertex weight to 0
-        
-        while len(Queue) > 0: # the main loop which checks  all the vertices
-            
-            self.bubble(Queue)
+        end_vertex = self.find_vertex(Queue, self.end)
+
+        while end_vertex in Queue: # the main loop which checks all the vertices
+            self.bubble(Queue) # sorts the queue
             
             current_vertex = Queue.pop(0) # similar to the dequeue in static languages
             current_vertex.permanent = True
 
-            print("\n\n {}".format(current_vertex.label))
-            for direction in dirList:
-                print("{}: {}".format(direction, current_vertex.adjacencyDict[direction]))
-            print(current_vertex.totalweight) 
-
             Permanent.append(current_vertex)
 
             for i in range(0,4): # iterates from 0 to 3, stops when at 4
-                dirVertexLabel = current_vertex.adjacencyDict[dirList[i]][0] # finds the vertex in the direction
+                dirVertex = self.find_vertex(Queue,current_vertex.adjacencyLabels[i]) # finds the vertex in the direction
 
-                if self.find_vertex(Queue, dirVertexLabel).totalweight > current_vertex.totalweight + current_vertex.adjacencyDict[dirList[i]][1]:
-                        self.find_vertex(Queue, dirVertexLabel).totalweight = current_vertex.totalweight + current_vertex.adjacencyDict[dirList[i]][1]
+                if dirVertex.totalweight > current_vertex.totalweight + current_vertex.adjacencyWeights[i]:
+                        dirVertex.totalweight = current_vertex.totalweight + current_vertex.adjacencyWeights[i]
+                        dirVertex.previousVertex = current_vertex.label
+                        dirVertex.previousDir = i
 
-   
-        
         # Output the final path to follow
-        # For now, this will just output all the vertices with the links
+        print(self.findPath(Permanent, self.end))
 
-        print("WOOOOOO, YOU GOT THIS FAR!!!!")
+    def findPath(self, final_list, curlabel, output = ""):
+        dirList = ["Left", "Right", "Rear", "Front"]
+        curVertex = self.find_vertex(final_list, curlabel)
+        output += "\n\nlabel: {},\nweight: {},\ndir: {}".format(curVertex.label, curVertex.totalweight, dirList[curVertex.previousDir])
+        if curVertex.label == "A":
+            return output
+        else:
+            return self.findPath(final_list,curVertex.previousVertex, output)
 
-        for item in Permanent:
-            print("\n\n {}".format(item.label))
-            for direction in dirList:
-                print("{}: {}".format(direction, item.adjacencyDict[direction]))
-            print(item.totalweight)             
+            
         
     def find_vertex(self, Queue, label):
-        for item in Queue:
-            if item.label == label:
-                return item # returns the vertex
+        if len(Queue)>0:
+            for item in Queue:
+                if item.label == label:
+                    return item # returns the vertex 
+        return self.nullVertex
 
-        try:
-            return Queue[len(Queue)-2]  #  returns the final value of the queue   
-        except:
-            print(len(Queue))
-            print(label)
-            for thing in Queue:
-                print(thing.label)
-            input()
-
+            
     def bubble(self, queue): # sorts the vertices by weight from start
             swaps = 1
             while swaps > 0:
@@ -118,7 +93,13 @@ class Graph():
             return queue # returns the sorted version of queue
 
 
-Test = Vertex("T")
-Test.update_adjacency(left=["A", 8.0])
+myNetwork = Graph("E")
+myNetwork.add_vertex(right=["B", 4])
+myNetwork.add_vertex(left=["A", 4], front=["C", 4])
+myNetwork.add_vertex(right=["E", 4], rear=["B", 4], front=["D", 4])
+myNetwork.add_vertex(rear=["D", 4])
+myNetwork.add_vertex(left=["C", 4])
 
-input()
+myNetwork.Dijkstras()
+
+print("reached the end")
