f21b67587460e747ccabc51fd1cc997779e07375
chilliconkearney <59411530+chilliconkearney@users.noreply.github.com>
2023-02-05 20:46:26 +0000

    update

---
+++ Car Upload/Logger.py
@@ -0,0 +1,16 @@
+import pickle
+import time
+
+class Logger:
+    def __init__(self) -> None:
+        self.filename = "Log"
+        self.interval = 0.1
+
+    def dump(self, object):
+        pass
+
+    def retrieve(self):
+        pass
+
+    def log(self):
+        pass
